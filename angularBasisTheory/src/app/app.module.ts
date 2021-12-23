85bdfc4ecd2c991ab1dde0308010645f679370b4
Michaelaudrup <maikelaudrupluisgonzalez@gmail.com>
2021-12-23 21:57:43 +0000

    mas teoria del curso de angular

--- angularBasisTheory/src/app/app.module.ts
+++ angularBasisTheory/src/app/app.module.ts
@@ -1,12 +1,19 @@
+/* ESTE ARCHIVO ES EL PRIMERO QUE SE EJECUTA AL LANZARSE LA APLICACION DE ANGULAR
+A NO SER QUE SE INDIQUE LO CONSTRIO EN EL ARCHIVO main.ts */
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [   //Se declaran los componentes que queremos que sean accesibles en toda la aplicacion
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
