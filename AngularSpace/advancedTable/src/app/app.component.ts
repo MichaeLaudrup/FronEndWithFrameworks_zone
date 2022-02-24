c8eb1371da9d37b4c26d67a51a3f53c00a3ae093
Michaelaudrup <maikelaudrupluisgonzalez@gmail.com>
2022-02-24 20:40:58 +0100

    mejorado proyecto del corte ingles

--- AngularSpace/advancedTable/src/app/app.component.ts
+++ AngularSpace/advancedTable/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advancedTable';
+  toLeft:boolean = false; 
+
+  moveApplicationLayout(){
+    this.toLeft = !this.toLeft;
+  }
 }
--- AngularSpace/advancedTable/src/app/header/header.component.ts
+++ AngularSpace/advancedTable/src/app/header/header.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  close:boolean = false; 
+  @Output('mobileMenuConmuted') mobileMenuConmuted: EventEmitter<any> = new EventEmitter(); 
+  menuOpen:boolean = false; 
   constructor() { }
 
   ngOnInit(): void {
   }
 
   conmuteClose(){
-    this.close = !this.close; 
+    this.menuOpen = !this.menuOpen; 
+    this.mobileMenuConmuted.emit(); 
   }
 }
